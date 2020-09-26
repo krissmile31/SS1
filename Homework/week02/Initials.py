69a6bf9bed91ce1d8840abfa4ed0656483582802
nguyenthingocanh8c <71318024+nguyenthingocanh8c@users.noreply.github.com>
2020-09-26 15:53:39 +0700

    Delete Initials.py

--- Homework/week02/Initials.py
+++
@@ -1,7 +0,0 @@
-fullName = input("Please enter your first, middle and last name, separated by a space: "  )
-name = fullName.split(" ")
-for i in range(len(name)):
-    name[i] = name[i][0:1]
-
-name = ". ".join(name)
-print(name.upper())
