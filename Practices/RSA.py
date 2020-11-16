0a611a14f6b263a64ae8f8d846b5fb29b25058d1
nguyenthingocanh8c <71318024+nguyenthingocanh8c@users.noreply.github.com>
2020-11-17 02:55:05 +0700

    Add files via upload

--- Practices/RSA.py
+++ Practices/RSA.py
@@ -31,10 +31,41 @@ def main():
     x = gcd(e, z)
     r = EEA(z, x[0])
     d = root(r,1, n, x[1])
-    c1, c2, c3 = 37, 39, 29
-    m1, m2, m3 = (c1**d) % n, (c2**d) % n, (c3**d) % n
-    print('The decrypted message: ', end="")
-    print(chr(m1), chr(m2), chr(m3))
+
+    while True:
+        print('Do u want encrypt or decrypt message [D/E]:')
+        s = input()
+        if s == 'D':
+            # use private key (n, d)
+            print('c1 = ', end ='')
+            c1 = int(input())
+            print('c2 = ', end ='')
+            c2 = int(input())
+            print('c3 = ', end ='')
+            c3 = int(input())
+
+            m1, m2, m3 = (c1**d) % n, (c2**d) % n, (c3**d) % n
+            print('The decrypted message: ', end="")
+            print(chr(m1), chr(m2), chr(m3))
+            break
+
+        elif s == 'E':
+            # use public key (n, e)
+            print('m1 = ', end ='')
+            m1 = int(input())
+            print('m2 = ', end ='')
+            m2 = int(input())
+            print('m3 = ', end ='')
+            m3 = int(input())
+            c1, c2, c3 = (m1 ** e) % n, (m2 ** e) % n, (m3 ** e) % n
+            print('The encrypted message: ', end="")
+            print(chr(c1), chr(c2), chr(c3))
+            break
+
+        else:
+            print('Invalid input~')
+            print()
+
 
 main()
 
